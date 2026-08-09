0a6cc22fe9f25c5286e568a0e96075ef7f447c79
okoriesam <okoriesamuel253@gmail.com>
2026-08-09 18:22:12 +0100

    Added Auto migration

--- LibraryManagementSystem.Api/Program.cs
+++ LibraryManagementSystem.Api/Program.cs
@@ -3,6 +3,8 @@ using LibraryManagementSystem.Application;
 using LibraryManagementSystem.Domain.Entity;
 using LibraryManagementSystem.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Identity;
+using LibraryManagementSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +22,9 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    await context.Database.MigrateAsync();
 
     var userManager = services.GetRequiredService<UserManager<User>>();
 
