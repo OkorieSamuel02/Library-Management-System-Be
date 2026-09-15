532042257d3b9e7a065514237deebc252a83008a
okoriesam <okoriesamuel253@gmail.com>
2026-09-15 11:56:06 +0100

    Modify my login response structure

---
+++ LibraryManagementSystem.Application/Authentication/DataTransferObject/Response/LoginResponseModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Application.Authentication.DataTransferObject.Response
+{
+    public class LoginResponseModel
+    {
+        public string Data { get; set; } = string.Empty; 
+        public UserDto User { get; set; } = new UserDto();
+    }
+
+    public class UserDto
+    {
+        public string email { get; set; } = string.Empty;
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string role { get; set; } = string.Empty;
+    }
+}
